072649e678ee3cb6242750159f58c51835a417a3
Santiago Lopez alba <santigolopezalba200228@gmail.com>
2023-05-25 22:29:04 -0500

    Noveno comit

--- Proyectos_react/HojaVida/segundo-react/src/App.js
+++ Proyectos_react/HojaVida/segundo-react/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+import miCara from './components/imagenes/WhatsApp Image 2023-05-22 at 22.24.10.jpeg';
+import OfMain from "./components/body";
+import OfMainAboutMeDes from "./components/bodyAboutMeDes";
+import logo from './components/imagenes/tecnologia.png';
+import OfMainTitles from './components/bodyMyTitles';
+import OfMainWorkExperience from './components/bodyExperiencesWorks';
+import OfMainHabilities from './components/bodyHabilities';
+import OfMainLenguages from './components/bodyLenguages';
+import OfMainFormulario from './components/bodyFormulario';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='fondo--main'>
+      <div className="container">
+        <div className="row">
+          <div className="col-5">
+            <br />
+            <img className="imagen--picture" src={miCara} alt=" " />
+            <OfMain className="main--letra" />
+            <br />
+            <br />
+            <br />
+            <img className="logos--aplicaciones" src={logo} alt=" " />
+          </div>
+          <div className="col-2"/>
+          <div className="col-5">
+            <div className="row">
+              <OfMainAcerca_de_Mi/>
+              <br/>
+              <OfMainTitulos/>
+              <br/>
+              <fMainExperienciaLaboral/>
+              <br/>
+              <OfMainHabilidades/>
+              <br/>
+              <OfMainLenguajes/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br/>
+      <br/>
+      <OfMainFormulario/>
     </div>
   );
 }
-
 export default App;
